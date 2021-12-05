f0d8e99e5a6f389e8108851323e143bd2648892c
Deniz Çağlarca <gambitmonkey1998@gmail.com>
2021-12-05 19:46:49 +0300

    Update forms.py
    Added forms

--- timetable/timetable_editor/forms.py
+++ timetable/timetable_editor/forms.py
@@ -20,3 +20,57 @@ class RoomForm(forms.ModelForm):
     class Meta:
         model = Room
         fields = ('capacity', 'description')
+
+class RoomListForm:
+    rooms = forms.ManyToManyField(required=True)
+
+    class Meta:
+        model = RoomList
+        fields = ('rooms')
+
+class SectionForm:
+    course = forms.ForeignKey(required=True)
+    current_section = forms.IntegerField(required=True)
+    total_section = forms.IntegerField(required=True)
+    instructor_id = models.ForeignKey(required=True)
+
+    class Meta:
+        model = Section
+        fields = ('course','current_section','total_section','instructor_id')
+
+class InstructorForm:
+    courses_given = forms.ManyToManyField(required=False)
+
+    class Meta:
+        model = Instructor
+        fields = ('courses_given')
+
+
+class StudentForm:
+    courses_taken = forms.ManyToManyField(required=False)
+
+    class Meta:
+        model = Student
+        fields = ('courses_taken')
+
+class EventForm:
+    course = forms.ForeignKey(required=True)
+    section = forms.ForeignKey(required=True)
+    length = forms.IntegerField(required=True)
+    description = forms.CharField(required=False)
+
+    class Meta:
+        model = Event
+        fields = ('course','section','length','description')
+
+class TimeTableForm:
+    days = forms.ManyToManyField(required=True)
+    starting_slot_perday = forms.IntegerField(required=True)
+    ending_slot_perday = forms.IntegerField(required=True)
+    slot_offset = forms.IntegerField(required=True)
+    events = forms.ManyToManyField(required=True)
+    rooms = forms.ManyToManyField(required=True)
+
+    class Meta:
+        model = TimeTable
+        fields = ('days','starting_slot_perday','ending_slot_perday','slot_offset','events','rooms')
