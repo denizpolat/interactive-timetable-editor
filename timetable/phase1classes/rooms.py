23ebd88037f132f55723df5e843e2d952efe4841
Deniz Çağlarca <gambitmonkey1998@gmail.com>
2021-12-06 22:07:19 +0300

    Final room classes

--- timetable/phase1classes/rooms.py
+++ timetable/phase1classes/rooms.py
@@ -1,33 +1,38 @@
+"""Id variable to make sure all created room and roomlists have a unique id"""
+room_id = 0
+roomlist_id = 0
+
 class Room():
     id = 0
     capacity = 0
     description = ""
 
-    def __init__(self, id, capacity, description):
-        self.id = id
+    def __init__(self, capacity, description):
+        global room_id
+        self.id = room_id
+        room_id += 1
         self.capacity = capacity
         self.description = description
 
-    def updateId(id):
-        self.id = id
-
-    def updateCapacity(capacity):
+    def updateCapacity(self, capacity):
         self.capacity = capacity
 
-    def updateDescription(description):
+    def updateDescription(self, description):
         self.desciption = desciption
 
 class RoomList():
     id = 0
     rooms = []
 
-    def __init__(self, id):
-        self.id = 0
+    def __init__(self):
+        global roomlist_id
+        self.id = roomlist_id
+        roomlist_id += 1
 
-    def addRoom(room_id):
+    def addRoom(self, room_id):
         if room_id not in rooms:
             rooms.append(room_id)
 
-    def removeRoom(room_id):
+    def removeRoom(self, room_id):
         if room_id in rooms:
-            rooms.append(room_id)
+            rooms.remove(room_id)
